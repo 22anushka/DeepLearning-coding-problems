2111a9c74e3c94bf28fa4b9775a836c2cd50c8a3
Anushka Sivakumar <61601761+22anushka@users.noreply.github.com>
2025-03-28 22:40:08 -0400

    Create temperature_sampling.py

---
+++ temperature_sampling.py
@@ -0,0 +1,11 @@
+# basically, scale the softmax exponent calculation probability by temperature to scale probability values towards one hot if temperature is low and towards uniform if temperature is high
+def temp_sampling(logits, t=0.6):
+
+  # convert logits to probs while scaling by temperature
+  stable_logits = np.max(logits, axis=-1, keep_dims=True)
+  t = 0.1 if t == 0 else t
+  temp_scaled = np.exp((logits-stable_logits)/t)/np.sum((logits-stable_logits)/t)
+
+  # can call sampling method given the probabilities from here
+  # greedy sampling
+  return np.random.choice(len(temp_scaled), p=temp_scaled)
