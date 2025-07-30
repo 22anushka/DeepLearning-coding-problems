e2327d0174b37fdc56aaf7be9f0b39c14cadb381
Anushka Sivakumar <61601761+22anushka@users.noreply.github.com>
2025-07-30 17:18:39 -0400

    Update temperature_sampling.py

--- temperature_sampling.py
+++ temperature_sampling.py
@@ -4,7 +4,7 @@ def temp_sampling(logits, t=0.6):
   # convert logits to probs while scaling by temperature
   stable_logits = np.max(logits, axis=-1, keep_dims=True)
   t = 0.1 if t == 0 else t
-  temp_scaled = np.exp((logits-stable_logits)/t)/np.sum((logits-stable_logits)/t)
+  temp_scaled = np.exp((logits-stable_logits)/t)/np.sum(np.exp((logits-stable_logits)/t), axis=-1)
 
   # can call sampling method given the probabilities from here
   # random sampling
